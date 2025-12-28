0f5b488d93162f1d4351cd433e8baadc7db4da63
Veljko <ivanisevic.sv44.2021@uns.ac.rs>
2025-12-29 00:26:55 +0100

    fix: Navbar fix.

--- app/src/main/java/com/example/gruber/activity/MainActivity.java
+++ app/src/main/java/com/example/gruber/activity/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
@@ -19,14 +20,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+//
+//        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+//
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottom_nav), (view, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            view.setPadding(0, systemBars.top, 0, systemBars.bottom);
+//            return insets;
+//        });
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
 
