91f37f891f888bf474481a005b14daa77d58ecef
umuskinja <muskinjau@gmail.com>
2026-01-15 14:05:26 +0100

    Extracting methods from HomeMapFragment to MapService (addition of MapService)

--- app/src/main/java/com/example/gruber/fragment/RideDetailsFragment.java
+++ app/src/main/java/com/example/gruber/fragment/RideDetailsFragment.java
@@ -268,7 +268,8 @@ public class RideDetailsFragment extends Fragment {
                     // Make it BLUE (use your palette: status_active)
                     int blue = ContextCompat.getColor(requireContext(), R.color.status_cancelled);
                     routeLine.getOutlinePaint().setColor(blue);
-                    routeLine.getOutlinePaint().setStrokeWidth(10f);
+                    routeLine.getOutlinePaint().setStrokeWidth(8f);
+                    routeLine.getOutlinePaint().setAntiAlias(true);
 
                     rideMap.getOverlays().add(routeLine);
 
@@ -302,7 +303,7 @@ public class RideDetailsFragment extends Fragment {
         m.setTitle(title);
 
         // try 26 for start/end, 22 for stops
-        int dp = (title.startsWith("Stop")) ? 44 : 48;
+        int dp = (title.startsWith("Stop")) ? 36 : 48;
         m.setIcon(getScaledMarker(iconRes, dp));
 
         // anchor after setting icon
